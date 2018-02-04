4c113565b19b6e54f05002dcad75c2298a60e064
天川之约 <woaitianwen@qq.com>
2018-02-04 21:16:54 +0800

    增加模糊查询

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const Promise = require("bluebird");
 const app = express();
 
-const port = process.env.PORT || 8080; 
+const port = process.env.PORT || 8080;
 
 //连接mysql数据库
 const connection = mysql.createConnection({
@@ -33,7 +33,7 @@ app.get('/goods', (req, res) => {
         "data": ""
     }
     queryAsync("select * from goods", (err, rows, fields) => {
-        if(rows.length !=0) {
+        if (rows.length != 0) {
             data["data"] = rows;
             res.json(data);
         } else {
@@ -47,63 +47,82 @@ app.get('/goods', (req, res) => {
 app.get('/good', (req, res) => {
     var total;
     var queryPagination;
-    var pageNum = parseInt(req.query.pageNum,10) || 1; // 每页码
-    var page = parseInt(req.query.page,10) || 0; // 默认页数
+    var pageNum = parseInt(req.query.pageNum, 10) || 1; // 每页码
+    var page = parseInt(req.query.page, 10) || 0; // 默认页数
     var numPages;
     var skip = page * pageNum;
-
+    var search = req.query.search;
+  
+  
     console.log(req.query);
     console.log(page);
     console.log(pageNum);
     console.log(skip);
+    console.log(search);
 
     var end = pageNum;
     var limit = skip + ',' + end;
 
     console.log(limit);
     console.log("SELECT * FROM goods DESC LIMIT " + limit);
-    queryAsync("select count(*) as total from goods")
-    .then(results => {
-        total = results[0].total;
-        numPages = Math.ceil(total / page);
-        console.log('number of pages:', numPages);
-    })
-        .then(() => queryAsync("SELECT * FROM goods LIMIT " + limit))
-        .then(results => {
-            var data = {
-                results: results
-            };
-            if(page > numPages) {
-                data.pagination = {
-                    current: page,        //当前页
-                    perPage: pageNum,  //每页码
-                    prev: page > 0 ? page - 1 : undefined,
-                    next: page < numPages - 1 ? page + 1: undefined
+    if(search) {
+       queryAsync("SELECT * FROM goods WHERE title like '%" + search + "%' LIMIT " + limit)
+            .then(results => {
+                var data = {
+                    results: results
+                };
+                res.json(data);
+            })
+    } else {
+        queryAsync("select count(*) as total from goods")
+            .then(results => {
+                total = results[0].total;
+                numPages = Math.ceil(total / page);
+                console.log('number of pages:', numPages);
+            })
+
+            .then(() => queryAsync("SELECT * FROM goods LIMIT " + limit))
+            .then(results => {
+                var data = {
+                    results: results,
+                    total: total
+                };
+
+                if (page > numPages) {
+                    data.pagination = {
+                        current: page,        //当前页
+                        perPage: pageNum,  //每页码
+                        prev: page > 0 ? page - 1 : undefined,
+                        next: page < numPages - 1 ? page + 1 : undefined
+                    }
+                } else {
+                    err: "查询页面" + page + "是 >= 最大页面号" + numPages
                 }
-            } else {
-                err: "查询页面"+ page +"是 >= 最大页面号" + numPages
-            }
-            res.json(data);
-        })
-        .catch(err => {
-            console.error(err);
-            res.json({ err: err });
-        });
-   
+
+                res.json(data);
+
+            })
+            .catch(err => {
+                console.error(err);
+                res.json({ err: err });
+            });
+    }
+    
+
 });
 
 //查询商品ID
 app.get('/good/:id', (req, res) => {
     var query = "select * from ?? where ?? = ?";
-    var table = ["goods","id",req.params.id];
-    query = mysql.format(query,table);
-    queryAsync(query, function(err, rows) {
-        if(err) {
+    var table = ["goods", "id", req.params.id];
+    query = mysql.format(query, table);
+    queryAsync(query, function (err, rows) {
+        if (err) {
             res.json({ error: true, message: "执行MySQL查询时出错" });
-        } else if(rows.length != 0){
+        } else if (rows.length != 0) {
             res.json({ error: false, message: "成功", "data": rows });
         } else {
-            res.json({ error: false, message: "无数据...."});
+            res.json({ error: false, message: "无数据...." });
         }
     })
 });
@@ -114,10 +133,10 @@ app.post('/good', (req, res) => {
     var price = req.body.price;
     var query = "insert into ??(??, ??) values(?,?)";
     var table = ["goods", "title", "price", title, price];
-    query = mysql.format(query,table);
-    queryAsync(query,function(err, rows) {
+    query = mysql.format(query, table);
+    queryAsync(query, function (err, rows) {
         if (err) {
-            res.json({ error: true, message: "执行MySQL查询时出错"});
+            res.json({ error: true, message: "执行MySQL查询时出错" });
         } else {
             res.json({ error: false, message: "成功创建商品" });
         }
@@ -161,10 +180,10 @@ app.listen(8080, () => {
 });
 
 function exitHandler(options, err) {
-  if (options.shutdownDb) {
-    console.log('shutdown mysql connection');
-    connection.end();
-  }
-  if (err) console.log(err.stack);
-  if (options.exit) process.exit();
+    if (options.shutdownDb) {
+        console.log('shutdown mysql connection');
+        connection.end();
+    }
+    if (err) console.log(err.stack);
+    if (options.exit) process.exit();
 }
