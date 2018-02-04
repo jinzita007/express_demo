639741e33f8f7177a7676839371222fe229e8f83
天川之约 <woaitianwen@qq.com>
2018-02-04 19:51:57 +0800

    重写ES6

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 var express = require("express");
 var mysql = require("mysql");
 var bodyParser = require("body-parser");
+var Promise = require("bluebird");
 var app = express();
 
 var port = process.env.PORT || 8080; 
 
 //连接mysql数据库
-var conn = mysql.createConnection({
+const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'mynode'
 });
 
+const queryAsync = Promise.promisify(connection.query.bind(connection));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // Body解析器使用JSON数据
 
-app.get('/', function(req, res) {
-    res.json({message:"欢迎来到API接口"});
-})
+app.get('/', (req, res) => {
+    res.json({ message: "欢迎来到API接口" });
+});
 //获取商品列表
-app.get('/goods', function(req,res) {
+app.get('/goods', (req, res) => {
     var data = {
         "data": ""
     }
-    conn.query("select * from goods", function(err, rows, fields){
+    queryAsync("select * from goods", (err, rows, fields) => {
         if(rows.length !=0) {
             data["data"] = rows;
             res.json(data);
@@ -36,19 +39,41 @@ app.get('/goods', function(req,res) {
 });
 
 //获取商品列表分页
-app.get('/good', function(req, res) {
-    var param = req.query || req.params;
-    var pageNum = parseInt(param.pageNum || 1);// 页码
-    var end = parseInt(param.pageSize || 3); // 默认页数
-    var start = (pageNum - 1) * end;
-    var query = "select * from ?? limit ?,?";
-    var table = ["goods", start, end];
-    query = mysql.format(query, table);
-    conn.query(query, function (err, rows, fields) {
-        if (err)
-        throw err;
-        res.json(rows)
-    });   
+app.get('/good', (req, res) => {
+    var total;
+    var pageNum = parseInt(req.query.pageNum,10) || 1; // 页码
+    var page = parseInt(req.query.page,10) || 0; // 默认页数
+    var numPages;
+    var skip = page * pageNum;
+
+    console.log(req.query);
+    console.log(page);
+    console.log(pageNum);
+    console.log(skip);
+
+    var end = pageNum;
+    var limit = skip + ',' + end;
+
+    console.log(limit);
+    console.log("SELECT * FROM goods DESC LIMIT " + limit);
+    queryAsync("select count(*) as total from goods")
+    .then(results => {
+        total = results[0].total;
+        numPages = Math.ceil(total / page);
+        console.log('number of pages:', numPages);
+    })
+        .then(() => queryAsync("SELECT * FROM goods LIMIT " + limit))
+        .then(results => {
+            var responsePayload = {
+                results: results
+            };
+            res.json(responsePayload);
+        })
+        .catch(err => {
+            console.error(err);
+            res.json({ err: err });
+        });
+   
 });
 
 //查询商品ID
@@ -56,7 +81,7 @@ app.get('/good/:id',function(req, res) {
     var query = "select * from ?? where ?? = ?";
     var table = ["goods","id",req.params.id];
     query = mysql.format(query,table);
-    conn.query(query, function(err, rows) {
+    queryAsync(query, function(err, rows) {
         if(err) {
             res.json({ error: true, message: "执行MySQL查询时出错" });
         } else if(rows.length != 0){
@@ -74,7 +99,7 @@ app.post('/good', function(req, res) {
     var query = "insert into ??(??, ??) values(?,?)";
     var table = ["goods", "title", "price", title, price];
     query = mysql.format(query,table);
-    conn.query(query,function(err, rows) {
+    queryAsync(query,function(err, rows) {
         if (err) {
             res.json({ error: true, message: "执行MySQL查询时出错"});
         } else {
@@ -91,7 +116,7 @@ app.put('/good/:id', function (req, res) {
     var query = "update ?? set ?? = ?,?? = ? where ?? = ?";
     var table = ["goods", "title", title, "price", price, "id", id];
     query = mysql.format(query, table);
-    conn.query(query, function (err, rows) {
+    queryAsync(query, function (err, rows) {
         if (err) {
             res.json({ error: true, message: "执行MySQL查询时出错" });
         } else {
@@ -106,7 +131,7 @@ app.delete("/good/:id", function (req, res) {
     var query = "delete from ?? where ?? = ?";
     var table = ["goods", "id", id];
     query = mysql.format(query, table);
-    conn.query(query, function (err, rows) {
+    queryAsync(query, function (err, rows) {
         if (err) {
             res.json({ error: true, message: "执行MySQL查询时出错" });
         } else {
